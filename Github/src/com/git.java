77c9c529e0597682959b37f2519c821f186c0cea
vaibhav2423 <94921364+vaibhav2423@users.noreply.github.com>
2026-06-23 21:22:00 +0530

    Update Git.java

--- Github/src/com/git.java
+++ Github/src/com/git.java
@@ -1,16 +1,12 @@
 package com;
 
-public class git {
+public class Git {
 	
 		
 		public static void main(String[] args) {
 			System.out.println("Hello");
 		}
-		
-		public void m() {
-			System.out.println("method d");
-			System.out.println("method A");
-			}
+	
 
 	}
 
